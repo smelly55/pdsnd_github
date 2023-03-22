798d406c58735a115026317113935871f9ea9cca
Melissa Trichilo <smelly55@gmail.com>
2023-03-22 20:28:56 +0800

    Added informative docstrings to ‘def load_data:’ section of code

--- bikeshare-project.py
+++ bikeshare-project.py
@@ -53,21 +53,37 @@ Returns:
 
 
 def load_data(city, month, day):
+
+"""
+Loads data for the specified city and filters by month and day if applicable.
+Args:
+(str) city - name of the city to analyze
+(str) month - name of the month to filter by, or "all" to apply no month filter
+(str) day - name of the day of week to filter by, or "all" to apply no day filter
+Returns:
+df - Pandas DataFrame containing city data filtered by month and day
+"""
+
+# load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
+# convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
+# extract month, day of week and hour from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     
     df['day_of_week'] = df['Start Time'].dt.day_name()
     
     df['hour'] = df['Start Time'].dt.hour
 
+# filter by month if applicable
     if month != 'all':
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
         df = df[df['month'] == month]
 
+# filter by day of week if applicable
     if day != 'all':
         df = df[df['day_of_week'] == day.title()]
         
